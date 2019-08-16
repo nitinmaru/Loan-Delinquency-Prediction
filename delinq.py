995058043bbabc4bf0f67b4f1daaa432e3d7e4f5
nitin maru <maru.nitin111@gmail.com>
2019-08-16 15:17:07 +0530

    Update delinq.py

--- delinq.py
+++ delinq.py
@@ -1,3 +1,8 @@
 """ this problrm was asked in hackathon for hiring ML ENgineer 
 https://datahack.analyticsvidhya.com/contest/india-ml-hiring-hackathon-2019/#
 Here we will discuss solution for the specific problrm"""
+
+import numpy as np
+
+print(np.__version__)
+
