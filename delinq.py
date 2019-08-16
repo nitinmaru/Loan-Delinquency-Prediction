a95cd22239e3ca4150c62101a9c452a414e84d87
nitin maru <maru.nitin111@gmail.com>
2019-08-16 13:31:37 +0530

    Create delinq.py

---
+++ delinq.py
@@ -0,0 +1,3 @@
+""" this problrm was asked in hackathon for hiring ML ENgineer 
+https://datahack.analyticsvidhya.com/contest/india-ml-hiring-hackathon-2019/#
+Here we will discuss solution for the specific problrm"""
